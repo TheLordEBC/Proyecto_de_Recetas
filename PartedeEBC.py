257417ae726bff6b84c7b1f01ed6938fb4b54585
TheLordEBC <ebaltazarcustodio@gmail.com>
2024-11-24 19:49:22 -0600

    Parte de EBC del día domingo 24 a las 19:49

---
+++ PartedeEBC.py
@@ -0,0 +1,17 @@
+# Pasos a seguir para empezar a trabajar en la rama Main
+# Si el otro hizo cambios debes hacer:
+#git pull origin main
+# Fin, ya puedes empezar a trabajar.
+
+#Para guardar tus cambios a la rama Main, debes hacer los siguientes pasos.
+#Primero guardar el archivo con: Ctrl + S
+#git add .
+#git commit -m "Aquí va cualquier mensaje"
+#git push origin main
+#Listo, ya acabaste de subir tus actualizaciones a la rama Main.
+
+#Como entrar a mi proyecto:
+#cd OneDrive
+#cd Universidad
+#cd "Programación Lógica y Funcional"
+print("Hello World, EBC, the only person in the World")
